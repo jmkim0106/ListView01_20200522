5abdd4fb997689d5bee12cff905136bbffc66c42
weekyeon <weekyeon@gmail.com>
2020-05-22 10:38:42 +0900

    User 클래스 추가  - getter / setter (private 변수)  - constructor (생성) 추가  - Alt + Insert 단축키로 자동 작성

---
+++ app/src/main/java/com/example/listview01_20200522/datas/user.java
@@ -0,0 +1,31 @@
+package com.example.listview01_20200522.datas;
+
+public class user {
+
+    private String name;
+    private String address;
+
+//    Alt + Insert
+
+
+    public user(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
